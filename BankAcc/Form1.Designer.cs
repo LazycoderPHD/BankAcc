424f8d6c05f13658cc18a3cd70693831b60e4437
LazycoderPHD <phatdao2309@gmail.com>
2026-03-23 20:21:52 +0700

    Added owner, amount, bankaccsgrid

--- BankAcc/Form1.Designer.cs
+++ BankAcc/Form1.Designer.cs
@@ -29,6 +29,12 @@
         private void InitializeComponent()
         {
             label1 = new Label();
+            label2 = new Label();
+            OwnerTxt = new TextBox();
+            AmountNum = new NumericUpDown();
+            BankAccsGrid = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)AmountNum).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)BankAccsGrid).BeginInit();
             SuspendLayout();
             // 
             // label1
@@ -37,20 +43,58 @@
             label1.Location = new Point(14, 18);
             label1.Margin = new Padding(5, 0, 5, 0);
             label1.Name = "label1";
-            label1.Size = new Size(63, 25);
+            label1.Size = new Size(72, 25);
             label1.TabIndex = 0;
-            label1.Text = "label1";
+            label1.Text = "Owner:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(14, 153);
+            label2.Margin = new Padding(5, 0, 5, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(83, 25);
+            label2.TabIndex = 1;
+            label2.Text = "Amount:";
+            // 
+            // OwnerTxt
+            // 
+            OwnerTxt.Location = new Point(94, 15);
+            OwnerTxt.Name = "OwnerTxt";
+            OwnerTxt.Size = new Size(186, 33);
+            OwnerTxt.TabIndex = 2;
+            // 
+            // AmountNum
+            // 
+            AmountNum.Location = new Point(105, 151);
+            AmountNum.Name = "AmountNum";
+            AmountNum.Size = new Size(145, 33);
+            AmountNum.TabIndex = 3;
+            // 
+            // BankAccsGrid
+            // 
+            BankAccsGrid.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            BankAccsGrid.Location = new Point(298, 18);
+            BankAccsGrid.Name = "BankAccsGrid";
+            BankAccsGrid.Size = new Size(327, 138);
+            BankAccsGrid.TabIndex = 4;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(11F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(472, 202);
+            ClientSize = new Size(656, 202);
+            Controls.Add(BankAccsGrid);
+            Controls.Add(AmountNum);
+            Controls.Add(OwnerTxt);
+            Controls.Add(label2);
             Controls.Add(label1);
             Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
             Margin = new Padding(5, 5, 5, 5);
             Name = "Form1";
             Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)AmountNum).EndInit();
+            ((System.ComponentModel.ISupportInitialize)BankAccsGrid).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -58,5 +102,10 @@
         #endregion
 
         private Label label1;
+        private Label label2;
+        private TextBox OwnerTxt;
+        private NumericUpDown numericUpDown1;
+        private NumericUpDown AmountNum;
+        private DataGridView BankAccsGrid;
     }
 }
